86f610abadff72032a508fa1775e26947eb5dcf7
Justin Barclay <git@justinbarclay.ca>
2026-03-25 12:34:06 -0700

    chore: npm run all

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,57 +1,52 @@
 // See: https://eslint.org/docs/latest/use/configure/configuration-files
-
-import { FlatCompat } from '@eslint/eslintrc'
-import js from '@eslint/js'
-import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import tsParser from '@typescript-eslint/parser'
-import jest from 'eslint-plugin-jest'
-import prettier from 'eslint-plugin-prettier'
-import globals from 'globals'
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all
-})
+});
 
 export default [
   {
-    ignores: ['**/coverage', '**/dist', '**/linter', '**/node_modules']
+    ignores: ["**/coverage", "**/dist", "**/linter", "**/node_modules"]
   },
   ...compat.extends(
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'plugin:prettier/recommended'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
   ),
   {
     plugins: {
-      jest,
       prettier,
-      '@typescript-eslint': typescriptEslint
+      "@typescript-eslint": typescriptEslint
     },
 
     languageOptions: {
       globals: {
         ...globals.node,
-        ...globals.jest,
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly'
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
       },
 
       parser: tsParser,
       ecmaVersion: 2023,
-      sourceType: 'module',
+      sourceType: "module",
 
       parserOptions: {
         projectService: {
           allowDefaultProject: [
-            '__fixtures__/*.ts',
-            '__tests__/*.ts',
-            'eslint.config.mjs',
-            'jest.config.js',
-            'rollup.config.ts'
+            "__fixtures__/*.ts",
+            "__tests__/*.ts",
+            "eslint.config.mjs",
+            "vitest.config.ts",
+            "rollup.config.ts"
           ]
         },
         tsconfigRootDir: import.meta.dirname
@@ -59,24 +54,24 @@ export default [
     },
 
     settings: {
-      'import/resolver': {
+      "import/resolver": {
         typescript: {
           alwaysTryTypes: true,
-          project: 'tsconfig.json'
+          project: "tsconfig.json"
         }
       }
     },
 
     rules: {
-      camelcase: 'off',
-      'eslint-comments/no-use': 'off',
-      'eslint-comments/no-unused-disable': 'off',
-      'i18n-text/no-en': 'off',
-      'import/no-namespace': 'off',
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-unused-vars': 'off',
-      'prettier/prettier': 'error'
+      camelcase: "off",
+      "eslint-comments/no-use": "off",
+      "eslint-comments/no-unused-disable": "off",
+      "i18n-text/no-en": "off",
+      "import/no-namespace": "off",
+      "no-console": "off",
+      "no-shadow": "off",
+      "no-unused-vars": "off",
+      "prettier/prettier": "error"
     }
   }
-]
+];
